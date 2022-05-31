8e0a31b368cacc4fda19ee95d2d4fc7a6a856084
Aviral Goel <aviralgoel@rocketmail.com>
2022-05-31 06:36:02 -0700

    commented and added more functionality

--- MAT 1 - 2 Player Battle Adventure/main.cpp
+++ MAT 1 - 2 Player Battle Adventure/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <memory>
+#include<cstdlib>
 using namespace std;
 
-//Game Class : It is responsible for displaying welcome text, rules and game over text. It is alwo responsible to assign heros to the player at the start of the gane.
-// The update method of the Game class will run as long as both players are alive and wait for each to play their turn.
+/* enum to store the types of available heros in the game */
 enum struct HeroTypes
 {
     Warlord,
@@ -11,36 +11,61 @@ enum struct HeroTypes
     Healer
 };
 
+/* Base class Hero, this class has the base attributes common to all the heroes, virtual functions which are defined in derived classes
+and some functions which are defined in the base class itself. */
 class Hero {
 
     protected:
-    int health;
-    int damage;
-    int healing;
-    int criticalHitDamage;
-    HeroTypes type;
+    int health; // health of the hero
+    int baseDamage; // base damage
+    int minHeal; // minimum amount of health healed by a hero
+    int maxHeal; // maximum amount of health a hero can heal himself
+    int minAdditionalDamage; // minimum amount of damage in addition to base damage
+    int maxAdditionalDamage; // maximum amount of damage in addition to base damage
+    HeroTypes type; // type of the hero
     public:
+    /* default constructor of the base class */
     Hero() {
         health = 0;
-        damage = 0;
-        healing  = 0;
-        criticalHitDamage = 0;
+        baseDamage = 0;
+        minHeal = 0;
+        maxHeal = 0;
+        minAdditionalDamage = 0;
+        maxAdditionalDamage = 0;
     }
+    /* getters */
     int getHeroHealth() {
         return health;
     }
     int getHeroDamage() {
-        return damage;
+        return baseDamage;
     }
     HeroTypes getHeroType() {
         return type;
     }
-    virtual ~Hero() {
+    /* virtual methods, implemented in the respective derived classes */
+    virtual int Attack() = 0; // method to calculate and return how much total (base+additional) damage to inflict on other player
+    virtual void IncreaseHealth() = 0; // method to calculate and return how much total (heal-range) health to increase for self
+    virtual void DecreaseHealth(int) = 0; // method to take given damage and decrease health by that much
+
+    /* non virtual methods, this logic is common to all the heros */
+
+    //calculate and return a random number between minHeal and maxHeal
+     int CalculateHealing() {
+        srand((unsigned) time(NULL));
+        int random = minHeal + (rand() % (maxHeal-minHeal));
+        return random;
     }
-    virtual int BaseAttack() = 0;
-    //virtual int SpecialAbility() = 0;
-    virtual void IncreaseHealth() = 0;
-    virtual void decreaseHealth(int) =0;
+    //calculate and return a random number between minAdditionalDamage and maxAdditionalDamage
+    int CalculateAdditionalDamage() {
+        srand((unsigned) time(NULL));
+        int random = minAdditionalDamage + (rand() % (maxAdditionalDamage-minAdditionalDamage));
+        return random;
+    }
+
+    /* default keyword means "It means that you want to use the compiler-generated version of that function, so you don't need to specify a body." */
+    /* this is because I do not have any additional code to put in the destructor */
+    virtual ~Hero() = default;
 
 };
 class Warlord: public Hero {
@@ -48,30 +73,34 @@ class Warlord: public Hero {
     public:
     Warlord() {
         health = 200;
-        damage = 25;
-        healing  = 10;
+        baseDamage = 20;
+        minHeal  = 10;
+        maxHeal = 15;
+        minAdditionalDamage = 5;
+        maxAdditionalDamage = 10;
         type = HeroTypes::Warlord;
 
     }
 
-    ~Warlord() {
-    }
-    int BaseAttack() {
-        int doDamanage = damage + criticalHitDamage;
+    int Attack() override
+    {
+
+        int doDamanage = baseDamage + CalculateAdditionalDamage();
+        cout << "Warlord slashes his sword across his enemy inflicting " << doDamanage << " damage!\n";
         return doDamanage;
     }
-    //virtual int SpecialAbility() = 0;
-    virtual void IncreaseHealth()
+    void IncreaseHealth() override
     {
-        cout << "Increasing health by " << healing << "\n";
-        health += healing;
+        cout << "Warlords decides to drink his healing potion...\n";
+        int _heal = CalculateHealing();
+        cout << "Health Increased by "<< _heal << "\n";
+        health += _heal;
     }
-    virtual void decreaseHealth(int _dmRcvd)
+    void DecreaseHealth(int _dmRcvd) override
     {
-    cout << "Decreasing health by " << _dmRcvd << "\n";
+        cout << "Warlord is attacked with " << _dmRcvd << " damage.\n";
         health-=_dmRcvd;
     }
-
 
 
 };
@@ -79,55 +108,64 @@ class Marksman: public Hero {
 
     public:
     Marksman() {
-        health = 100;
-        damage = 45;
-        healing  = 15;
+        health = 150;
+        baseDamage = 25;
+        minHeal  = 5;
+        maxHeal = 10;
+        minAdditionalDamage = 10;
+        maxAdditionalDamage = 15;
         type = HeroTypes::Marksman;
     }
-    ~Marksman() {
-
-    }
-    virtual void IncreaseHealth()
+    void IncreaseHealth() override
     {
-        cout << "Increasing health by " << healing << "\n";
-        health += healing;
+        cout << "Marksman decides to pop some pills...\n";
+        int _heal = CalculateHealing();
+        cout << "Health Increased by "<< _heal << "\n";
+        health += _heal;
     }
-    virtual void decreaseHealth(int _dmRcvd)
+    void DecreaseHealth(int _dmRcvd) override
     {
     cout << "Decreasing health by " << _dmRcvd << "\n";
         health-=_dmRcvd;
     }
-    int BaseAttack() {
-        int doDamanage = damage + criticalHitDamage;
+    int Attack() override
+    {
+        int doDamanage = baseDamage + CalculateAdditionalDamage();
+         cout << "Marksman takes his shot and pierces the enemy's body doing " << doDamanage << " damage!\n";
         return doDamanage;
     }
-
-
 };
 class Healer: public Hero {
 
+
     public:
     Healer() {
-        health = 150;
-        damage = 15;
-        healing  = 20;
+        health = 120;
+        baseDamage = 15;
+        minHeal  = 15;
+        maxHeal = 20;
+        minAdditionalDamage = 10;
+        maxAdditionalDamage = 15;
         type = HeroTypes::Healer;
 
     }
-    ~Healer() {
-    }
-    virtual void IncreaseHealth()
+
+    void IncreaseHealth() override
     {
-        cout << "Increasing health by " << healing << "\n";
-        health += healing;
+         cout << "Healer decides to summon the energy of the dead...\n";
+        int _heal = CalculateHealing();
+        cout << "Health Increased by"<< _heal << "\n";
+        health += _heal;
     }
-    virtual void decreaseHealth(int _dmRcvd)
+    void DecreaseHealth(int _dmRcvd) override
     {
-    cout << "Decreasing health by " << _dmRcvd << "\n";
+        cout << "Decreasing health by " << _dmRcvd << "\n";
         health-=_dmRcvd;
     }
-    int BaseAttack() {
-        int doDamanage = damage + criticalHitDamage;
+    int Attack() override
+    {
+        int doDamanage = baseDamage + CalculateAdditionalDamage();
+        cout << "Healer slams his bonesaw to the enemy's head, whoosh its  " << doDamanage << " damage!\n";
         return doDamanage;
     }
 
@@ -205,24 +243,50 @@ class Game {
     }
     public: void Update()
     {
-        while(hero1->getHeroHealth() > 0 && hero2->getHeroHealth()>0)
+        while(true)
         {
-            int player1Health = hero1->getHeroHealth();
-            int player2Health = hero2->getHeroHealth();
-            cout << "Player 1 Health: " << player1Health << endl;
-            cout << "Player 2 Health: " << player2Health << endl;
+            cout << "The Battle Begins..!\n";
+            GetPlayerHealths();
+            ActiveGame();
+            char ch;
+            do {
+                cout << "The Battle Ends..!\nDo you want to play again? (Y/N)";
+                cin >> ch;
+
+            }while(ch!='Y' && ch!='N');
+            if(ch=='N')
+                return;
+            HeroSelection();
+            HeroAssign();
+            DeclareHeros();
+        }
+
+    }
+    private: void ActiveGame()
+    {
+    while(hero1->getHeroHealth() > 0 && hero2->getHeroHealth()>0)
+        {
+
             cout << "Player 1: Play your turn.\nPress (H) to Heal or Press (D) to Damage\n";
             char player1choice; char player2choice;
             do {
-
                 cin >> player1choice;
             }while(player1choice!='H' && player1choice!='D');
-            if(player1choice=='H')
+            if(player1choice=='H') {
                 hero1->IncreaseHealth();
+                GetPlayerHealths();
+            }
             if(player1choice=='D')
             {
-                int damage = hero1->BaseAttack();
-                hero2->decreaseHealth(damage);
+                int damage = hero1->Attack();
+                hero2->DecreaseHealth(damage);
+                GetPlayerHealths();
+                if(hero2->getHeroHealth()<=0)
+                {
+                    DisplayLoseText(2);
+                    break;
+                }
+
             }
             cout << "Player 2: Play your turn.\nPress (H) to Heal or Press (D) to Damage\n";
             do {
@@ -230,13 +294,34 @@ class Game {
                 cin >> player2choice;
             }while(player2choice!='H' && player2choice!='D');
              if(player2choice=='H')
+             {
+
                 hero2->IncreaseHealth();
+                GetPlayerHealths();
+             }
+
             if(player2choice=='D')
             {
-                int damage = hero2->BaseAttack();
-                hero1->decreaseHealth(damage);
+                int damage = hero2->Attack();
+                hero1->DecreaseHealth(damage);
+                GetPlayerHealths();
+                if(hero1->getHeroHealth()<=0)
+                {
+                    DisplayLoseText(1);
+                    break;
+                }
+
             }
         }
+    }
+    private: void GetPlayerHealths()
+    {
+        cout << "Player 1 Health: " << hero1->getHeroHealth() << endl;
+        cout << "Player 2 Health: " << hero2->getHeroHealth() << endl;
+    }
+    private: void DisplayLoseText(int _playerNumber)
+    {
+        cout << "Player " << _playerNumber << " has fallen !!\n. They will haunt the dungeon forever...\n";
     }
 };
 
@@ -245,6 +330,7 @@ int main()
     Game game;
     game.Start();
     game.Update();
+    //Warlord wr; Healer hr; Marksman mr;
 
     return 0;
 }
