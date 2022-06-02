358ce6118fa2a66103aba37eb8c777545108ca2a
Aviral Goel <aviralgoel@rocketmail.com>
2022-06-03 03:11:46 +0400

    added level 1

--- MAT - II - Text-based RPG Adventure Game/main.cpp
+++ MAT - II - Text-based RPG Adventure Game/main.cpp
@@ -2,20 +2,64 @@
 #include <memory>
 #include <map>
 #include <string>
+
 using namespace std;
 
 class Actor {
 
     protected:
     int health;
+    int meleeDamange;
     public:
     virtual ~Actor() = default;
+    void setHealth(int _health)
+    {
+        health = _health;
+    }
+    int getHealth()
+    {
+        return health;
+    }
+    void TakeDamage(int _damage)
+    {
+        health -= _damage;
+    }
+    virtual int attackMelee() = 0;
+    virtual int Heal() = 0;
+
 };
 
 class Enemy: public Actor {
-    int damage;
+
     public:
-    virtual ~Enemy() = default;
+    Enemy () = default;
+    Enemy(int _currentLevel)
+    {
+        cout << "A Enemy has spawned near you!\n";
+        health = 50 + (5*_currentLevel)/2;
+        meleeDamange = 5 + (5*_currentLevel)/2;
+
+    }
+   // public:
+    virtual ~Enemy() {
+        cout <<"The enemy has been killed\n";
+    };
+    char RandomChoice()
+    {
+        srand(time(0));
+        return (rand() % 5) ? 'M' : 'H'; // 20% chance of H 80% of M
+    }
+    int attackMelee() override
+    {
+        cout << "The enemy fights back with " << meleeDamange << " damage.";
+        return meleeDamange;
+    }
+    int Heal() override {
+        cout << "Behold! The enemy drank his healing potion!\n";
+        health += 5;
+        return 0;
+    }
+
 };
 
 class BossEnemy: public Enemy {
@@ -30,9 +74,9 @@ class BossEnemy: public Enemy {
 
 class Hero: public Actor {
 
-    int meleeDamange;
     int rangedDamage;
     int defence;
+    int currentLevel;
     map<string, int> specialAbilities;
     map<string, bool> gearsOfArms;
 
@@ -42,6 +86,7 @@ class Hero: public Actor {
         meleeDamange = 10;
         rangedDamage = 5;
         defence = 5;
+        currentLevel = 1;
         specialAbilities = {{"CriticalHit", false},{"Blocker", true}, {"LifeSteal", false}, {"RangedAttack", false}};
         gearsOfArms = {{"Sword", false},{"Shield", false}, {"Armour", false}, {"Bow", false}};
     }
@@ -65,6 +110,29 @@ class Hero: public Actor {
         }
 
     }
+    void setCurrentLevel(int _level)
+    {
+        currentLevel = _level;
+    }
+    int getCurrentLevel ()
+    {
+        return currentLevel;
+    }
+    int attackMelee() override
+    {
+        cout << "Your hero slams his weapon across the enemy's face\n";
+        return meleeDamange;
+    }
+    int attackRanged()
+    {
+        cout << "Your hero sneaks a ranged projectile towards the enemy!\n";
+        return rangedDamage;
+    }
+    int Heal() override {
+        cout << "Behold! The hero summons the spirits and heals himself!\n";
+        health += (currentLevel * 10);
+        return 0;
+    }
 
 
 };
@@ -79,8 +147,13 @@ class Game {
     }
     public: void Update()
     {
-        //isplayPlayerStats();
-        //DisplayGameStats();
+        if(!player)
+        {
+            cout << "Player is has not been spawned correctly!\n";
+            return;
+        }
+        player->DisplayPlayerStats();
+        LoadLevel(2);
     }
     private: void Monologue()
     {
@@ -99,16 +172,80 @@ class Game {
     private: void SpawnPlayer()
     {
         player = make_unique<Hero>();
-        player->DisplayPlayerStats();
     }
+    private: void LoadLevel(int _currentPlayerLevel)
+    {   int temp = _currentPlayerLevel;
+        cout <<"You have stepped in to level 1.\n";
+        cout <<"Tread lighty, danger awaits at every corner!\n";
+        shared_ptr<Enemy> e1 = make_shared<Enemy>(_currentPlayerLevel);
+        temp--;
+        while(player->getHealth() > 0 && e1->getHealth() > 0)
+        {
+            cout <<"Quick Press H (Heal) or M (Melee Attack) or R (Ranged Attack)\n";
+            char ch;
+            do {
+                cin >> ch;
+            } while(ch!='H' && ch!='M' && ch!='R');
+            int damageToEnemy = PlayerChoice(ch);
+            e1->TakeDamage(damageToEnemy);
+            int damageToPlayer = EnemyChoice(e1->RandomChoice(), e1);
+            player->TakeDamage(damageToPlayer);
+            cout << "Player Health is: " << player->getHealth() << endl;
+            cout << "Enemy Health is: " << e1->getHealth() << endl;
+            if(temp && e1->getHealth() <= 0)
+            {
+                e1.reset();
+                e1 = make_shared<Enemy>(_currentPlayerLevel);
+                temp--;
+            }
 
+        }
+
+
+    }
+    private: int PlayerChoice(char _ch)
+    {
+        if(_ch=='H')
+        {
+            player->Heal();
+            return 0;
+        }
+        else if(_ch=='M')
+        {
+            return player->attackMelee();
+        }
+        else
+        {
+            return player->attackRanged();
+        }
+
+    }
+     private: int EnemyChoice(char _ch, shared_ptr<Enemy> actor)
+    {
+        if(_ch=='M')
+        {
+            return actor->attackMelee();
+        }
+        else
+        {
+            return actor->Heal();
+        }
+    }
 
 };
 
 int main()
 {
-    Game game;
-    game.Start();
+   Game game;
+   game.Start();
+   game.Update();
+//    Hero hero;
+//    cout << hero.getHealth();
+    //nemy e1(1);
+    //cout << e1.RandomChoice();
+
+
+
 
     return 0;
 }
